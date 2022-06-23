d5dadf1735b11cf6d4e3546a8bf211fba5d792bb
hunter850 <hunter850711@gmail.com>
2022-06-23 12:01:50 +0800

    package.json script帶入參數

--- src/12.dotenv.js
+++ src/12.dotenv.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
-const { DB_HOST, DB_PORT, DB_USER, DB_PASS} = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS, NODE_ENV} = process.env;
 
-console.log({DB_HOST, DB_PORT, DB_USER, DB_PASS});
+console.log({DB_HOST, DB_PORT, DB_USER, DB_PASS, NODE_ENV});
