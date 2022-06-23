16cb131188a75925473223f48309c112b0dee9b6
hunter850 <hunter850711@gmail.com>
2022-06-23 12:05:12 +0800

    prettier

--- src/12.dotenv.js
+++ src/12.dotenv.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
-const { DB_HOST, DB_PORT, DB_USER, DB_PASS, NODE_ENV} = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS, NODE_ENV } = process.env;
 
-console.log({DB_HOST, DB_PORT, DB_USER, DB_PASS, NODE_ENV});
+console.log({ DB_HOST, DB_PORT, DB_USER, DB_PASS, NODE_ENV });
