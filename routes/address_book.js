463143f0806e9b97caa397f15032180a3c88b9c8
hunter850 <hunter850711@gmail.com>
2022-06-29 11:13:50 +0800

    import date tools

--- routes/address_book.js
+++ routes/address_book.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require(__dirname + "/../modules/connect_db");
 
 const router = express.Router(); // 建立 router 物件
+const { toDateString } = require(__dirname + "/../modules/date_tools");
 
 router.get("/", async (req, res) => {
     let output = {
@@ -13,23 +14,23 @@ router.get("/", async (req, res) => {
     }
 
     let page = req.query.page || 1;
-    if(page < 1) return res.redirect("?page=1");
+    if (page < 1) return res.redirect("?page=1");
 
     const sql = "SELECT COUNT(1) totalRows FROM address_book";
-    const [[{totalRows}]] = await db.query(sql);
+    const [[{ totalRows }]] = await db.query(sql);
 
     let totalPages = 0;
-    if(totalRows) {
+    if (totalRows) {
         totalPages = Math.ceil(totalRows / output.perPage);
-        if(page > totalPages) return res.redirect(`?page=${totalPages}`);
+        if (page > totalPages) return res.redirect(`?page=${totalPages}`);
 
         const sql2 = `SELECT * FROM address_book LIMIT ${(page - 1) * output.perPage}, ${output.perPage}`;
         const [result] = await db.query(sql2);
+        result.forEach(el => el.birthday = toDateString(el.birthday));
         output.rows = result;
     }
 
-    output = {...output, page, totalPages, totalRows}
+    output = { ...output, page, totalPages, totalRows }
     res.render("address_book/main", output);
 })
-
 module.exports = router;
