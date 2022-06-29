1919098f0e88b3047c2f0b022cf4b84864ceeaa4
hunter850 <hunter850711@gmail.com>
2022-06-29 10:23:11 +0800

    create address book.js

---
+++ routes/address_book.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const db = require(__dirname + "/../modules/connect_db");
+
+const router = express.Router(); // 建立 router 物件
+
+router.get("/", async (req, res) => {
+    let output = {
+        perPage: 10,
+        page: 1,
+        totalRows: 0,
+        totalPages: 0,
+        rows: []
+    }
+
+    let page = req.query.page || 1;
+    if(page < 1) return res.redirect("?page=1");
+
+    const sql = "SELECT COUNT(1) totalRows FROM address_book";
+    const [[{totalRows}]] = await db.query(sql);
+
+    let totalPages = 0;
+    if(totalRows) {
+        totalPages = Math.ceil(totalRows / output.perPage);
+        if(page > totalPages) return res.redirect(`?page=${totalPages}`);
+
+        const sql2 = `SELECT * FROM address_book LIMIT ${(page - 1) * output.perPage}, ${output.perPage}`;
+        const [result] = await db.query(sql2);
+        output.rows = result;
+    }
+
+    output = {...output, page, totalPages, totalRows}
+    res.render("address_book/main", output);
+})
+
+module.exports = router;
